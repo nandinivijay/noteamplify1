3cc47ba0c616b2f3608d53bcac0dad1a978f08a7
Nandini.rajaram <nandinivijayr@gmail.com>
2022-11-29 18:54:40 +0000

    updated code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import { NavBar,  NoteUICollection} from './ui-components'
-
+import { useState } from 'react'
 function App() {
+    const [showCreateModal, setShowCreateModal] = useState(false)
+  const [showUpdateModal, setShowUpdateModal] = useState(false)
+  const [updateNote, setUpdateNote] = useState()
   return (
     <>
     <NavBar width="100%" marginBottom='20px' />
      <div className='container'>
       <NoteUICollection />
       </div>
-         <div className='modal' style={{display: 'none'}}>
-        <CreateNote/>
-      </div>
-      <div className='modal' style={{display: 'none'}}>
-        <UpdateNote />
-      </div>
+        
     </>
   );
 }
